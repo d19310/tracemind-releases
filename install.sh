ac4ba3feb02bc3d21a41d8719d331f16bb812c96
Vincent Dong <d19310@gmail.com>
2026-05-26 17:53:04 +0800

    Release TraceMind 2.1.5

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ PLUGIN_ID="tracemind"
 PLUGIN_NAME="TraceMind"
 GITHUB_REPO="d19310/tracemind-releases"
 
-DEFAULT_VERSION="v2.1.4"
+DEFAULT_VERSION="v2.1.5"
 
 # Resolve version: env var > local manifest.json > DEFAULT_VERSION
 if [[ -n "${TRACEMIND_VERSION:-}" ]]; then
